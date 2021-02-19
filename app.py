eb616c991a1012a1a5c51e43488d9934f6a93e75
richardthorp <richardthorp@hotmail.co.uk>
2021-02-19 09:32:52 +0000

    Add index view function and template

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask_pymongo import PyMongo
-from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask import (Flask, flash, render_template,
+                   redirect, request, session, url_for)
 from flask_paginate import Pagination, get_page_args
 if os.path.exists("env.py"):
     import env
@@ -13,6 +14,14 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-test = list(mongo.db.recipes.find())
 
-print(test)
+@app.route("/")
+def index():
+    test = mongo.db.recipes.find()
+    return render_template("index.html", test=test)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
