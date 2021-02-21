8af958aa9612e22e373a3e2edfd21f123ad3280f
richardthorp <richardthorp@hotmail.co.uk>
2021-02-21 13:41:26 +0000

    Add placeholder text and length validation to reg form

--- forms.py
+++ forms.py
@@ -7,11 +7,19 @@ from wtforms.fields.html5 import EmailField
 class Registration_form(FlaskForm):
     email = EmailField("Email Address",
                        validators=[Email(),
-                                   InputRequired()])
+                                   InputRequired()], 
+                       render_kw={"placeholder": "Email Address"})
     username = StringField("Username",
-                           validators=[InputRequired(), Length(min=4, max=25)])
+                           validators=[InputRequired(), Length(min=4, max=25)],
+                           render_kw={"placeholder": "Choose Username",
+                                      "minlength": "4",
+                                      "maxlength": "25"})
     password = PasswordField("Password",
-                             validators=[InputRequired(), Length(min=8)])
+                             validators=[InputRequired(), Length(min=8)],
+                             render_kw={"placeholder": "Choose Password",
+                                        "minlength": "8"})
     confirm_password = PasswordField("Confirm Password", validators=[
-                                     InputRequired(), EqualTo('password')])
+                                     InputRequired(), EqualTo('password')],
+                                     render_kw={"placeholder":
+                                                "Retype Password"})
     submit = SubmitField("Register!")
